5b015cda85ebfc23107f899fe0069ee871ee8c57
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-07 08:04:55 +0000

    datatypes

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+//"use strict";// treat all js code as newer version
+//alert("hello") // we are using nodejs, not browser
+// code readiability should be high
+
+// let name = "Umar"
+// let age = 24
+// let isLoggedIn = false
+
+// namber => 2 to power 53
+// bigint for very large number
+// String => ""
+// Boolean => true/false
+// null => standalone value / Object
+// undefined => undefined
+// symbol => unique
+
+
+// object =>
+    console.log(typeof "Umar");
+    
