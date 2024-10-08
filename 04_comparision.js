9dd4e6796e9ec4abb48cf5a342dbeb82ee98159e
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-08 08:39:48 +0000

    comparision

---
+++ 04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2"> 1);
+// console.log(2>"1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // it shows true because in javascript comparision operators converts null to 0.
+//
+
+
+
