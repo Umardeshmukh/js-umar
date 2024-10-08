956894552194eb564e84cba3a8d319348b2190bc
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-08 08:20:41 +0000

    operations and conversions

--- .devcontainer/01_basics/03_conversionOprations.js
+++ .devcontainer/01_basics/03_conversionOprations.js
@@ -11,5 +11,49 @@ let score = "33"
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+//******************************Operations ********************************** */
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log("1" + 2);
+// console.log(1 +"2");
+// console.log("1" + 2 + 2); // If string come first all the values considered as string
+// console.log(1 + 2 + "2"); // If string comes last then the arithmatic operation will be done on first values and then concatinated with string which is last .
+
+// console.log(+true);
+// console.log(+"");
+// let num1 ,num2, num3 
+// num1 = num2 = num3 = 2+ 2
+ let gameCounter = 100
+ gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+ 
+
+ let value = 3 
+ let negValue = -value
+ //console.log(negValue);
+ 
+ // console.log("1" + 2);
+ // console.log(1 +"2");
+ // console.log("1" + 2 + 2); // If string come first all the values considered as string
+ // console.log(1 + 2 + "2"); // If string comes last then the arithmatic operation will be done on first values and then concatinated with string which is last .
+ 
+ // console.log(+true);
+ // console.log(+"");
+ // let num1 ,num2, num3 
+ // num1 = num2 = num3 = 2+ 2
+  let gameCounter = 100
+  gameCounter = 100
+  gameCounter++;
+  console.log(gameCounter);
+  
+ 
+ 
+ 
+ 
+ 
 
 
