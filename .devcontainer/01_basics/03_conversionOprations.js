a73636786eee916579b956e490b0614d45956430
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-08 07:26:20 +0000

    conversion of data type

---
+++ .devcontainer/01_basics/03_conversionOprations.js
@@ -0,0 +1,15 @@
+let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+//"33"=> 33
+// "33abc"=> NaN(Not a number)
+// true => 1; false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
