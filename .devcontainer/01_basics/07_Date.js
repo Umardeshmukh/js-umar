cbc7def39103ed27b3dfe3a04a69490ea67d31ef
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-16 14:16:29 +0530

    Create 07_Date.js

---
+++ .devcontainer/01_basics/07_Date.js
@@ -0,0 +1,24 @@
+// Dates
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCrteatedDate = new Date (2023,0,24)
+// let myCrteatedDate = new Date ("2023,01,24")
+
+let myCrteatedDate = new Date ("01-19-2024")
+// console.log(myCrteatedDate.toDateString());
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCrteatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+ let newDate = new Date()
+//  console.log(newDate.getMonth() + 1);
+  
+
+newDate.toLocaleString('default',{
+    weekday : "long"
+})
+ console.log(newDate);
+ 
