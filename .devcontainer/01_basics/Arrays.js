97f5f15f4df2e14b1a7759cfed1541c13a001f2b
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-10-18 17:14:29 +0530

    Create Arrays.js

---
+++ .devcontainer/01_basics/Arrays.js
@@ -0,0 +1,80 @@
+//array
+
+const mArr=[0,1,2,3,4,]
+// console.log(mArr[1]); 
+
+// mArr.push(10)
+// mArr.pop()
+// mArr.unshift(09)
+// mArr.shift()
+// console.log(mArr.includes(9));
+// console.log(mArr.indexOf(3));
+// const newArr = mArr.join()
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice, splice
+console.log("A", mArr);
+const myn1= mArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", mArr);
+
+const myn2= mArr.splice(0,3)
+console.log("C", mArr);
+
+// console.log(myn1);
+console.log(myn2);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
