b4ee89dda8f167e5889625ac79a3f58e852e00e8
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2024-12-05 16:52:26 +0530

    Update 2_scopes.js

--- .devcontainer/02_basics/2_scopes.js
+++ .devcontainer/02_basics/2_scopes.js
@@ -1,1 +1,45 @@
+let a = 10 
+const b= 20
+var c = 30 
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+
+function one() {
+    const username = "umros"
+
+    function two()
+    {
+        const websitr ="youtube"
+         //console.log(username);
+        
+    }
+    two()
+}
+one()
+if (true ){
+    const username = "umros"
+    if (username === "umros"){
+        const website = "youtube"
+       // console.log(username + website);
+        
+    }
+     // console.log(website);
+      
+}
+//console.log( username);
+
+
+//  ++++++++++++++++  interesting +++++++++++++++++
+
+function addone (num) {
+   return num + 1
+}
+console.log(addone(5));
+addTwo(5) 
+const addTwo = function (num ){
+    return num +2
+}
+
+ 
